d151fb4ff097e540f6ac72267802575cf79dd551
Alex_Fike <alexfike2016@gmail.com>
2022-06-10 17:30:41 +0500

    tast 5 in homework seminar 2

---
+++ Homework_seminar_5/Task2.py
@@ -0,0 +1,17 @@
+import posixpath 
+
+def read_file_list(link):
+    file = open(link, 'r')
+    read_file = file.read() + ''
+    list_numbers = list(map(int, read_file.split()))
+    file.close()
+    return list_numbers
+
+def check(A):
+    for i in range(A[0],len(A)+1):
+        if A[i] - 1 != A[i -1]:
+            result = i+1
+            break
+    return result
+
+print(check(read_file_list('Homework_seminar_5/file2.txt')))
